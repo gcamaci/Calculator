d8c0e3a848368e157edb0bb0de4111453f8898a2
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-03-19 18:55:26 -0500

    added UI

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let operator = '';
 let arg1 = '';
 let arg2 = '';
 let answer = 0;
-const display = document.querySelector('display.num')
+const displayb = document.querySelector('.display-num')
 const clear = document.querySelector('#clear');
 const solution = document.querySelector('#equals');
 const operators = document.querySelectorAll('.operator');
+
+
+//for each op button check the globals and change the operator as needed
+
+
 operators.forEach((op) => {
     op.addEventListener('click',() => {
         if(arg1==='' && answer ==0){
@@ -14,31 +19,31 @@ operators.forEach((op) => {
         }else if(arg1 !== '' && arg2 !== ''){
             calculate(operator,arg1,arg2);
             operator = op.value;
-            console.log(`${answer} ${operator}`)
+            displayb.textContent = `${answer} ${operator}`
         
         }else{
             operator = op.value;
-            console.log(`${arg1} ${operator}`)
-
+            displayb.textContent = `${arg1} ${operator}`
+            
         }
     });
 });
-
+//number buttons, creating argument 1 and 2. 
 const buttons = document.querySelectorAll('.buttons');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if(operator ===''){
             console.log(button.value);
             arg1 += button.value;
-            console.log(`argument one: ${arg1}`); 
+            displayb.textContent = `${arg1}`
+             
         }else{
             arg2 += button.value;
-            console.log(`${arg1} ${operator} ${arg2} `);
+            displayb.textContent = `${arg1} ${operator} ${arg2} `
+            
         }
     });
-
 });
-
 function add (a,b){
     return answer = a + b;
 };
@@ -49,13 +54,20 @@ function multi (a,b){
     return answer = a * b;
 };
 function divi(a,b){
-    return answer = a / b;
+    if(b===0){
+        console.log("cant divide by zero")
+    }else{
+        return answer = a / b;
+    }
+    
 };
 solution.addEventListener('click', () =>{
     calculate(operator,arg1,arg2);
+    displayb.textContent = `${answer}`
     console.log(`${answer}`)
 })
 function clearDisplay (){
+    displayb.textContent ="0"
     operator = '';
     arg1 = '';
     arg2 = '';
@@ -67,9 +79,13 @@ function calculate (oper, argu1, argu2){
     let num1 = parseFloat(argu1);
     let num2 = parseFloat(argu2);
     if(oper === "+"){
-        answer = add(num1,num2);
+        answer = add(num1,num2).toFixed(2);
     }else if(oper==='-'){
-        answer = sub(num1,num2);
+        answer = sub(num1,num2).toFixed(2);
+    }else if(oper === '/'){
+        answer = divi(num1,num2).toFixed(2);
+    }else{
+        answer = multi(num1,num2).toFixed(2);
     }
     arg1 = answer;
     arg2 = ''
