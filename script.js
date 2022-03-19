4e6b779ee6b3ca7f3c4d102283c94ddbd1db97b2
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-03-18 22:48:03 -0500

    figure out operator answer

---
+++ script.js
@@ -0,0 +1,63 @@
+let operator = '';
+let arg1 = '';
+let arg2 = '';
+let answer = 0;
+
+const clear = document.querySelector('#clear');
+const solution = document.querySelector('#equals');
+const operators = document.querySelectorAll('.operator');
+operators.forEach((op) => {
+    op.addEventListener('click',() => {
+        if(arg1==='' && answer ==0){
+            console.log("chose value first");
+            operator = '';
+        }else{
+            operator = op.value;
+            console.log(operator);
+        }
+    });
+
+
+});
+
+const buttons = document.querySelectorAll('.buttons');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(operator ===''){
+            console.log(button.value);
+            arg1 += button.value;
+            console.log(`argument one: ${arg1}`); 
+        }else{
+            arg2 += button.value;
+            console.log(`${arg1} ${operator} ${arg2} `);
+        }
+    });
+
+});
+
+function add (a,b){
+    return answer = a + b;
+};
+function multi (a,b){
+    return answer = a * b;
+};
+function divi(a,b){
+    return answer = a /b;
+};
+solution.addEventListener('click', () =>{
+    let num1 = parseFloat(arg1);
+    let num2 = parseFloat(arg2);
+    answer = add(num1,num2);
+    arg1 = answer;
+    arg2 = ''
+    console.log(answer)
+})
+function clearDisplay (){
+    operator = '';
+    arg1 = '';
+    arg2 = '';
+    answer = 0;
+}
+clear.addEventListener('click',clearDisplay);
+
+//left off trying to make the operator act as =.
