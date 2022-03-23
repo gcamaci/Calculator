71e835c4565cb9057b02729fa9c37f85f07bd128
gcamaci <77690094+gcamaci@users.noreply.github.com>
2022-03-22 19:41:55 -0500

    tidy2

--- script.js
+++ script.js
@@ -3,32 +3,50 @@ let arg1 = '';
 let arg2 = '';
 let answer = 0;
 
-
 const displayb = document.querySelector('.display-num')
 const clear = document.querySelector('#clear');
 const solution = document.querySelector('#equals');
 const operators = document.querySelectorAll('.operator');
 
+function calculate(oper,argu1,argu2){
+    let num1 = parseFloat(argu1);
+    let num2 = parseFloat(argu2);
+    switch(oper){
+        case '+':
+            answer = num1 + num2;
+        break;
+        case '-':
+            answer = num1 - num2;
+        break;
+        case '*':
+            answer = num1 * num2;
+        break;
+        case '/':
+            answer = num1 / num2;
+        break;
+    }
+};
+const chooseOp = (e) => {
+    if(arg1===''){
+        operator = '';
+    }else if(arg1 !== '' && arg2 !== ''){
+        calculate(operator,arg1,arg2);
+        arg1 = answer;
+        arg2 = ''
+        operator = e.target.value;
+        displayb.textContent = `${answer} ${operator}`
+    
+    }else{
+        operator = e.target.value;
+        displayb.textContent = `${arg1} ${operator}`;
+    }
 
+};
 //for each op button check the globals and change the operator as needed
 operators.forEach((op) => {
-    op.addEventListener('click',() => {
-        if(arg1===''){
-            operator = '';
-        }else if(arg1 !== '' && arg2 !== ''){
-            calculate(operator,arg1,arg2);
-            arg1 = answer;
-            arg2 = ''
-            operator = op.value;
-            displayb.textContent = `${answer} ${operator}`
-        
-        }else{
-            operator = op.value;
-            displayb.textContent = `${arg1} ${operator}`
-            
-        }
-    });
+    op.addEventListener('click',chooseOp);
 });
+
 //number buttons, creating argument 1 and 2. 
 const buttons = document.querySelectorAll('.buttons');
 buttons.forEach((button) => {
@@ -62,23 +80,7 @@ clear.addEventListener('click', () => {
     arg2 = '';
     answer = 0;
 });
-function calculate(oper,argu1,argu2){
-    let num1 = parseFloat(argu1);
-    let num2 = parseFloat(argu2);
-    switch(oper){
-        case '+':
-            answer = num1 + num2;
-        break;
-        case '-':
-            answer = num1 - num2;
-        break;
-        case '*':
-            answer = num1 * num2;
-        break;
-        case '/':
-            answer = num1 / num2;
-    }
-};
+
 const backSpace = document.querySelector('.backspace');
 backSpace.addEventListener('click', () =>{
     arg1 = arg1.toString();
